67e624da33d328431c7297fe808a10db6af440fe
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-25 23:28:08 +0530

    sending res

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("Content-Header", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page!</title></head>");
+  res.write("<body><h1>Hello From Node Js!!!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
