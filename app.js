163335a970660533589860e8371c0e35156ce86a
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-26 23:17:43 +0530

    sending response

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === "/") {
     res.write("<html>");
@@ -10,6 +12,13 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message' method='POST'> <input type='text' name='message' /> <button type='submit'>Submit</button> </form></body>"
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Header", "text/html");
