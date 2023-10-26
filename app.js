f635ab5372ffcb56ca4ae3a6dc1ed62100c9afc9
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-26 23:28:08 +0530

    Parsing Request Bodies

--- app.js
+++ app.js
@@ -16,7 +16,17 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "Dummy");
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFileSync("message.txt", message);
+    });
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
