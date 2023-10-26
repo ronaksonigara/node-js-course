217df06142f0acb2f252b5e2dbe01ddbc88d4e9b
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-26 23:11:00 +0530

    routing request

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'> <input type='text' name='message' /> <button type='submit'>Submit</button> </form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Header", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page!</title></head>");
