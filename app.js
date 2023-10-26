61f6b8cb11523c785cb3d576c8cd4b06368b4f48
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-26 23:33:01 +0530

    Understanding Event Driven Code Execution

--- app.js
+++ app.js
@@ -21,15 +21,14 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
 
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-
       fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
   res.setHeader("Content-Header", "text/html");
   res.write("<html>");
