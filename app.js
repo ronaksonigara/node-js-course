f127669bdc20560d083d638c0e943213952abf42
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-27 23:37:24 +0530

    Serving Files Statically

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRouter);
 app.use(shopRouter);
 
