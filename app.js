cbb526012ed8b2b9f3bcc2f54c6d8726fef6991c
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-27 21:42:30 +0530

    Adding Middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("I'm in middleware");
+  next(); // Allows the request to continue to next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("I'm in another middleware");
+  // ....
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
