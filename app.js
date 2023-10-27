9d91960a0dfb0f5fdd1d6a687e595cb4309a033f
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-27 22:44:45 +0530

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const shopRouter = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(adminRouter);
 app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
