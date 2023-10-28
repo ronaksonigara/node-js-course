41d4cc663da5c0acf8cf94db38827520b0f15f18
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-28 15:37:28 +0530

    Working with Embedded JavaScript Templating (EJS) & Working on the Layout with Partials

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const { routes } = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 
 const app = express();
 
-app.engine(
-  ".hbs",
-  expressHbs.engine({
-    extname: ".hbs",
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-  })
-);
-app.set("view engine", ".hbs");
+// app.engine(
+//   ".hbs",
+//   expressHbs.engine({
+//     extname: ".hbs",
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//   })
+// );
+// app.set("view engine", ".hbs");
 
 // app.set("view engine", "pug");
+
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
