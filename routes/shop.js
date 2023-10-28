e5a5924c354ea0609c74cc15299359a400ef664b
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-10-29 01:47:06 +0530

    Extracting Dynamic Params

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const express = require("express");
 const {
   getIndex,
   getProducts,
+  getProduct,
   getCart,
   getOrders,
   getCheckout,
@@ -13,6 +14,8 @@ const router = express.Router();
 router.get("/", getIndex);
 
 router.get("/products", getProducts);
+
+router.get("/product/:productId", getProduct);
 
 router.get("/cart", getCart);
 
