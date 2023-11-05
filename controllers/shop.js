56acadfae4a04622e6a3ff16032a9eab8a4304fe
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-11-05 23:52:15 +0530

    Resetting the Cart and Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -147,9 +147,11 @@ exports.postCart = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -166,6 +168,9 @@ exports.postOrder = (req, res, next) => {
           );
         })
         .then(() => {
+          return fetchedCart.setProducts(null);
+        })
+        .then(() => {
           res.redirect("/orders");
         })
         .catch((error) => {
@@ -179,10 +184,18 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
