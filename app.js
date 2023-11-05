5d708e862576f5887474a1d617721c526104f936
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-11-05 22:22:28 +0530

    One-To-Many and Many-To-Many Relations

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const { get404 } = require("./controllers/error");
 
@@ -43,9 +45,15 @@ Product.belongsTo(User, {
 });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
   })
