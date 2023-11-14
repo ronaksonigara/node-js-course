5194b2d1c1604243db7cf784adeae3957f6126b5
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-11-14 23:00:05 +0530

    Creating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,22 +14,27 @@ exports.postAddProduct = (req, res, next) => {
   const price = +req.body.price;
   const description = req.body.description;
 
-  req.user
-    .createProduct({
-      title: title,
-      imageUrl: imageUrl,
-      price: price,
-      description: description,
-      // userId: req.user.id,
-    })
+  const product = new Product(title, price, description, imageUrl);
 
-    // Product.create({
-    //   title: title,
-    //   imageUrl: imageUrl,
-    //   price: price,
-    //   description: description,
-    //   // userId: req.user.id,
-    // })
+  // req.user
+  //   .createProduct({
+  //     title: title,
+  //     imageUrl: imageUrl,
+  //     price: price,
+  //     description: description,
+  //     // userId: req.user.id,
+  //   })
+
+  // Product.create({
+  //   title: title,
+  //   imageUrl: imageUrl,
+  //   price: price,
+  //   description: description,
+  //   // userId: req.user.id,
+  // })
+
+  product
+    .save()
     .then((result) => {
       console.log("Created Product");
       res.redirect("/");
