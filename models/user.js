8647814f7d3165e0d2ce495bdcf53bd9db9a0d6d
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-11-15 19:44:20 +0530

    Working on Cart Items and Orders

--- models/user.js
+++ models/user.js
@@ -2,14 +2,30 @@ const { ObjectId } = require("mongodb");
 const { getDb } = require("../util/database");
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart) {
     this.name = name;
     this.email = email;
+    this.cart = cart; // {  items: []  }
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new ObjectId(this._id) },
+      {
+        $set: { cart: updatedCart },
+      }
+    );
   }
 
   static findById(userId) {
