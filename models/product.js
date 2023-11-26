6c8f9524f03557c32a52cdedf29166fae3838508
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-11-26 22:55:35 +0530

    Creating the Product Schema

--- models/product.js
+++ models/product.js
@@ -1,5 +1,23 @@
-const { ObjectId } = require("mongodb");
-const { getDb } = require("../util/database");
+const { Schema } = require("mongoose");
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
 
 class Product {
   constructor(title, price, description, imageUrl, _id, userId) {
