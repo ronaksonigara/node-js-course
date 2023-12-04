206fc55cd16386e35b619f7883970cb14b914fe1
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2023-12-04 23:48:16 +0530

    Loading the Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,8 +43,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then((products) => {
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
@@ -54,31 +55,6 @@ exports.getCart = (req, res, next) => {
     .catch((error) => {
       console.log(error);
     });
-
-  //   Product.fetchAll((products) => {
-  //     const cartProducts = [];
-  //     for (let product of products) {
-  //       const cartProductData = cart.products.find(
-  //         (cartProduct) => cartProduct.id === product.id
-  //       );
-  //       if (cartProductData) {
-  //         cartProducts.push({
-  //           ...product,
-  //           quantity: cartProductData.quantity,
-  //         });
-  //       }
-  //     }
-
-  //     res.render("shop/cart", {
-  //       path: "/cart",
-  //       pageTitle: "Your Cart",
-  //       cart: {
-  //         ...cart,
-  //         products: cartProducts,
-  //       },
-  //     });
-  //   });
-  // });
 };
 
 exports.postCart = (req, res, next) => {
